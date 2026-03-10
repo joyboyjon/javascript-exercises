9d2d5be6e9dbf3334771e44c32da212974f3eb19
joyboyjon <155998630+joyboyjon@users.noreply.github.com>
2026-03-10 12:54:38 +0200

    Javascript exercises 06 to 11

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+    let sum=0;
+    if(!Number.isInteger(a) || !Number.isInteger(b) || a<0 || b<0) return "ERROR";
 
+    if(a>b){
+        aux=a;
+        a=b;
+        b=aux;
+    }
+    for(let i=a;i<=b;i++)
+        sum+=i;
+    return sum;
 };
 
 // Do not edit below this line
--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,5 +1,10 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    const divByFour=year%4===0;
+    const isCentury= year%100===0;
+    const divBy400 = year%400===0;
 
+    if(divByFour && (!isCentury || divBy400)) return true;
+    else return false;
 };
 
 // Do not edit below this line
